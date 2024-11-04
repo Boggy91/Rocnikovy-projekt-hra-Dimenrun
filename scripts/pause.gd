0a51a0ce2244652cb3581c1dbbaf89bf39d89713
Boggy91 <bogdansaenz@gmail.com>
2024-11-04 21:30:36 +0100

    kook

--- scripts/pause.gd
+++ scripts/pause.gd
@@ -12,9 +12,16 @@ func _process(delta: float) -> void:
 func _on_resume_pressed() -> void:
 	pause_panel.hide()
 	get_tree().paused= false
+	
+
 
 
 func _on_menu_pressed() -> void:
 	get_tree().paused= false
 	get_tree().change_scene_to_file("res://scenes/Main_menu.tscn")
 	
+
+
+func _on_restart_lvl_pressed() -> void:
+	get_tree().paused= false
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
