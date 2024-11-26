f83f8cc793acc2541090c22551cc4280af76737a
Boggy91 <bogdansaenz@gmail.com>
2024-11-26 12:42:49 +0100

    nove

--- dialogue/main_menu.gd
+++ dialogue/main_menu.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	get_tree().change_scene_to_file("res://scenes/lvl_menu.tscn")
 
 
 
---
+++ scripts/lvl_menu.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Main_menu.tscn")
+
+
+func _on_lvl_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Level 1.tscn")
