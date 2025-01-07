db76ad83221cbb81d1dac59ef3b3537edd1ae9a7
Boggy91 <bogdansaenz@gmail.com>
2025-01-07 23:35:53 +0100

    nove

--- scripts/scoreboard.gd
+++ scripts/scoreboard.gd
@@ -2,16 +2,21 @@ extends Node
 
 @onready var score_list_container: VBoxContainer = $"ScrollContainer/VBoxContainer"
 
+# Load the custom font resource
+const font_file = preload("res://fonts/Audex-Regular.ttf")
+
 func _ready() -> void:
 	populate_scores()
 
 func populate_scores() -> void:
 	var best_scores = Global.get_scoreboard()
 
+	# Clear existing children
 	for child in score_list_container.get_children():
 		score_list_container.remove_child(child)
 		child.queue_free()
 
+	# Populate the scoreboard with new entries
 	for entry in best_scores:
 		if entry != null:
 			var score_label = Label.new()
@@ -19,11 +24,16 @@ func populate_scores() -> void:
 				entry["level"], 
 				entry["points"], 
 				entry["time"]
-			 ]
+			]
+
+			# Apply the custom font and size
+			if font_file:
+				var custom_font := FontFile.new()
+				custom_font.font_data = font_file
+				score_label.add_theme_font_override("font", custom_font)
+				score_label.add_theme_font_size_override("font",30)
 
 			score_list_container.add_child(score_label)
-
-
 
 func _on_level_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/GAME/lvl_menu.tscn")
